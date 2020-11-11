542cbbd79d0cb1a6273555d388003b0c869c0f45
Chad Saglam <saglam.chd@gmail.com>
2020-11-11 12:33:01 +0100

    kontrol ediver gari

--- src/Bank.java
+++ src/Bank.java
@@ -2,6 +2,6 @@ public class Bank {
   public static void main(String[] args) {
     System.out.println("TESTIM LOW!!!");
     System.out.println("bayramım ben");
-
+    System.out.println("Anlasildi mi?");
   }
 }
